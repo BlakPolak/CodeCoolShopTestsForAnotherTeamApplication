427622016e36f8bde673a989fe2410841e3ad97d
Kamil <mika.kamil12@gmail.com>
2017-05-02 11:59:58 +0200

    Move display list to ProductController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -9,18 +10,8 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<>();
-        ProductCategory category = new ProductCategory("Category", "Department", "Description");
-        Supplier supplier = new Supplier("Supplier", "Description");
-        Product product1 = new Product("Product", 12.50f, "PLN", "Description", category, supplier);
-        Product product2 = new Product("Product 2", 12.50f, "PLN", "Description", category, supplier);
-        Product product3 = new Product("Product 3", 12.50f, "PLN", "Description", category, supplier);
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        for(Product product : products){
-            System.out.println(product.toString());
-        }
+        ProductController productController = new ProductController();
+        productController.displayList();
     }
 
 
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,12 +2,32 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProductController {
+
+    public void displayList(){
+        List<Product> products = new ArrayList<>();
+        ProductCategory category = new ProductCategory("Category", "Department", "Description");
+        Supplier supplier = new Supplier("Supplier", "Description");
+        Product product1 = new Product("Product", 12.50f, "PLN", "Description", category, supplier);
+        Product product2 = new Product("Product 2", 12.50f, "PLN", "Description", category, supplier);
+        Product product3 = new Product("Product 3", 12.50f, "PLN", "Description", category, supplier);
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        for(Product product : products){
+            System.out.println(product.toString());
+        }
+    }
 
 
 }
