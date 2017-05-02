c321dd0556abca39a51883249610822c9cd99ea4
Kamil <mika.kamil12@gmail.com>
2017-05-02 13:27:14 +0200

    Create displyList method in ProductView in order to display product List

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,17 +3,18 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
 
 import java.util.List;
 
 public class ProductController {
+    private ProductDao productDao = new ProductDaoSqlite();
+    private ProductView view = new ProductView();
 
     public void displayList(){
-        ProductDao productDao = new ProductDaoSqlite();
         List<Product> products = productDao.getAll();
-        for(Product product : products){
-            System.out.println(product.toString());
-        }
+        view.displayList(products);
+
     }
 
 
--- src/main/java/com/codecool/shop/view/ProductView.java
+++ src/main/java/com/codecool/shop/view/ProductView.java
@@ -1,4 +1,14 @@
 package com.codecool.shop.view;
 
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
 public class ProductView {
+
+    public void displayList(List<Product> products){
+        for(Product product : products){
+            System.out.println(product.toString());
+        }
+    }
 }
