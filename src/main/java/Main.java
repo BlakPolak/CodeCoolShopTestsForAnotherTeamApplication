c451670fa428131b0e67af9814aa793b3b489a55
Kamil <mika.kamil12@gmail.com>
2017-05-04 10:08:29 +0200

    Create menu and switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.SqliteJDBCConnector;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 public class Main {
+    static ProductController productController = new ProductController();
 
     public static void main(String[] args) {
         if(args.length > 0 && args[0].equals("--create-tables")){
@@ -14,10 +16,25 @@ public class Main {
             }catch (SQLException e){
                 e.printStackTrace();
             }
-
         }
-        ProductController productController = new ProductController();
-        productController.displayList();
+
+        System.out.println("1. List all products");
+        System.out.println("2. List products by category");
+        System.out.println("Select option");
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()){
+            System.out.println("Wrong input try again");
+            scanner.next();
+        }
+        Integer option = scanner.nextInt();
+        switch (option){
+            case 1:
+                productController.displayList();
+            case 2:
+                break;
+            default:
+                System.out.println("Wrong option");
+        }
     }
 
 
