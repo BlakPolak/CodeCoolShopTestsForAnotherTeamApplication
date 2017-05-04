97463a35be7205d5134881fe8edb1aca298fd248
Kamil <mika.kamil12@gmail.com>
2017-05-04 13:45:28 +0200

    create SupplierDaoSqlite and pass description in constructor

---
+++ src/main/java/com/codecool/shop/dao/SupplierDaoSqlite.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class SupplierDaoSqlite implements SupplierDao {
+    @Override
+    public void add(Supplier supplier) {
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        return null;
+    }
+}
