af511e2fbfb3d3be1a1ea6cfd5316651e833a24b
Kamil <mika.kamil12@gmail.com>
2017-05-04 14:05:20 +0200

    create method listProductsBySupplier

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -4,8 +4,11 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductCategoryDaoSqlite;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.SupplierDaoSqlite;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import com.codecool.shop.view.ProductView;
 import com.codecool.shop.view.UserInput;
 
@@ -16,6 +19,7 @@ public class ProductController {
     private ProductDao productDao = new ProductDaoSqlite();
     private ProductView view = new ProductView();
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
+    private SupplierDao supplierDao = new SupplierDaoSqlite();
 
     public void displayList(){
         List<Product> products = productDao.getAll();
@@ -28,6 +32,15 @@ public class ProductController {
         Integer categoryId = UserInput.getUserInput();
         ProductCategory category = productCategoryDao.find(categoryId);
         List<Product> products = productDao.getBy(category);
+        view.displayList(products);
+    }
+
+    public void listProductBySupplier(){
+        List<Supplier> suppliers= supplierDao.getAll();
+        view.displaySupplierList(suppliers);
+        Integer supplierID = UserInput.getUserInput();
+        Supplier supplier = supplierDao.find(supplierID);
+        List<Product> products = productDao.getBy(supplier);
         view.displayList(products);
     }
 
