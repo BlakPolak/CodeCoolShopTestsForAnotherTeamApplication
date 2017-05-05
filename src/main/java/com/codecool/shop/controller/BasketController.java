078168b37f63e7be8cefabd836184569e1e8858f
Kamil <mika.kamil12@gmail.com>
2017-05-05 10:11:17 +0200

    create method add to cart

--- src/main/java/com/codecool/shop/controller/BasketController.java
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -1,7 +1,23 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.model.Basket;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
+import com.codecool.shop.view.UserInput;
+
+import java.util.List;
+
 public class BasketController {
+    private ProductDao productDao = new ProductDaoSqlite();
+    private ProductView productView = new ProductView();
+    private Basket basket = new Basket();
+
     public void addToCartAction(){
-        System.out.println("Add to cart");
+        List<Product> products = this.productDao.getAll();
+        Integer userInput = UserInput.getUserInput();
+        Product product = productDao.find(userInput);
+        basket.add(product, 1);
     }
 }
