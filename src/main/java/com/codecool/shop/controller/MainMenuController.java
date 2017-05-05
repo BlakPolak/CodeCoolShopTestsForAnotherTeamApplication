e3fbe9dffec75432ca5fb0837076719ddd844f44
Kamil <mika.kamil12@gmail.com>
2017-05-05 11:00:08 +0200

    call display basket

--- src/main/java/com/codecool/shop/controller/MainMenuController.java
+++ src/main/java/com/codecool/shop/controller/MainMenuController.java
@@ -11,6 +11,7 @@ public class MainMenuController {
         System.out.println("2. List products by category");
         System.out.println("3. List products by supplier");
         System.out.println("4. Add product to cart");
+        System.out.println("5. Display Basket");
         System.out.println("Select option");
         Integer option = UserInput.getUserInput();
         switch (option){
@@ -25,6 +26,10 @@ public class MainMenuController {
                 break;
             case 4:
                 this.basketController.addToCartAction();
+                break;
+            case 5:
+                this.basketController.displayBasketItem();
+                break;
             default:
                 System.out.println("Wrong option");
         }
