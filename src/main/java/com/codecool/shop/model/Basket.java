9232942d994be9d4aaca32b0fffa4fd496ddf784
Kamil <mika.kamil12@gmail.com>
2017-05-05 10:12:37 +0200

    Create add method

--- src/main/java/com/codecool/shop/model/Basket.java
+++ src/main/java/com/codecool/shop/model/Basket.java
@@ -6,4 +6,28 @@ import java.util.List;
 public class Basket {
     private List<BasketItem> items = new ArrayList<>();
 
+    public List<BasketItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<BasketItem> items) {
+        this.items = items;
+    }
+
+    public void add(Product product, Integer quantity){
+        Boolean productExists = false;
+        for(BasketItem item: this.getItems()){
+            if(item.getProduct().getId() == product.getId()){
+                item.setQuantity(item.getQuantity() + quantity);
+                productExists = true;
+            }
+        }
+        if(!productExists){
+            BasketItem basketItem = new BasketItem(product, quantity);
+            this.getItems().add(basketItem);
+        }
+    }
+
+
+
 }
