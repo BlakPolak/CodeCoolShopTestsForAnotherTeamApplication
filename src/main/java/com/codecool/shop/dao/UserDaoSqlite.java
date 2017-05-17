f3e67efab3147487e1d08d586e7ef59a148f6121
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-17 13:03:44 +0200

    Create UserDaoSqlite with empty methods

---
+++ src/main/java/com/codecool/shop/dao/UserDaoSqlite.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.User;
+
+import java.util.List;
+
+/**
+ * Created by pati on 17.05.17.
+ */
+public class UserDaoSqlite implements UserDao {
+    
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public User find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
