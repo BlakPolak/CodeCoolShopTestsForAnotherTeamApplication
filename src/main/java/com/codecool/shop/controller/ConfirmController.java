2744b0ada5c1ca9ba96f358b2a0109f31afe5fbf
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-17 11:09:12 +0200

    Harcode basket to test confirm template

--- src/main/java/com/codecool/shop/controller/ConfirmController.java
+++ src/main/java/com/codecool/shop/controller/ConfirmController.java
@@ -1,5 +1,7 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.model.Basket;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -13,10 +15,25 @@ import java.util.Map;
  */
 public class ConfirmController {
 
+    private static Basket basket = null;
+    private ProductDaoSqlite productDaoSqlite = new ProductDaoSqlite();
+
     public String displayConfirmForm(Request req, Response res) {
         Map params = new HashMap<>();
+        params.put("basket", getBasket());
         ModelAndView render = new ModelAndView(params, "product/confirm");
         return new ThymeleafTemplateEngine().render(render);
+
+    }
+
+    private Basket getBasket() {
+        if (basket.equals(null)) {
+            basket = new Basket();
+            basket.add(productDaoSqlite.find(1), 5);
+            basket.add(productDaoSqlite.find(2), 10);
+            basket.add(productDaoSqlite.find(3), 15);
+        }
+        return basket;
 
     }
 
