38835cf9a98054fb0b71bda85daa77bb4e1708c1
cj <sigamateusz@gmail.com>
2017-05-17 11:41:02 +0200

    Add BasketController() to constructor, create before() & get("/basket")

--- src/main/java/com/codecool/shop/Application.java
+++ src/main/java/com/codecool/shop/Application.java
@@ -1,12 +1,20 @@
 package com.codecool.shop;
 
 
+import com.codecool.shop.controller.BasketController;
 import com.codecool.shop.dao.SqliteJDBCConnector;
+import com.codecool.shop.model.Basket;
+import spark.Request;
+import spark.Response;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static spark.Spark.before;
 import static spark.Spark.exception;
+import static spark.Spark.get;
+import static spark.Spark.halt;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
 
@@ -14,9 +22,12 @@ public class Application {
 
     private static Application app = new Application();
     private Connection connection;
+    private BasketController basketController = null;
 
 
-    private Application() { }
+    private Application() {
+        basketController = new BasketController();
+    }
 
     public static Application getApp() {
         return app;
@@ -41,8 +52,21 @@ public class Application {
     }
 
     public void appRoutes(){
+        before((request, response) -> {
+            boolean newSession = request.session().isNew();
+            // ... check if authenticated
+            if (newSession) {
+                Basket basket = new Basket();
+                request.session().attribute("basket",basket);
+            }
+        });
 
+        get("/basket", (Request req, Response res) -> {
+            Basket basket = req.session().attribute("basket");
+            return basketController.renderBasket(basket);
+        });
     }
+
     public static void run(){
         Application.getApp().setConnection();
         Application.getApp().appSettings();
