12273c0db2448572d8dfbf7821f60c51f5ce0fc7
cj <sigamateusz@gmail.com>
2017-05-17 11:38:31 +0200

    Delete private Basket & displayBasketItem(), create renderBasket()

--- src/main/java/com/codecool/shop/controller/BasketController.java
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -7,16 +7,19 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.view.BasketView;
 import com.codecool.shop.view.ProductView;
 import com.codecool.shop.view.UserInput;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BasketController {
     private ProductDao productDao = new ProductDaoSqlite();
     private BasketView basketView = new BasketView();
-    private Basket basket = new Basket();
     private ProductView productView = new ProductView();
 
-    public void addToCartAction(){
+    public void addToCartAction(Basket basket){
         List<Product> products = this.productDao.getAll();
         productView.displayList(products);
         Integer userInput = UserInput.getUserInput();
@@ -25,7 +28,9 @@ public class BasketController {
         basketView.displayBasket(basket.getItems());
     }
 
-    public void displayBasketItem() {
-        basketView.displayBasket(basket.getItems());
+    public String renderBasket(Basket basket) {
+        Map params = new HashMap<>();
+        params.put("basket", basket);
+        return new ThymeleafTemplateEngine().render(new ModelAndView(params, "product/basket"));
     }
 }
