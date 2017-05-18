abc9ff222f68b94cb3a0c2b78ac48e6721efc164
cj <sigamateusz@gmail.com>
2017-05-18 17:44:42 +0200

    Change modal html when click

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -4,19 +4,22 @@
 $(document).ready(function(){
 
     $(".add-to-basket").click(function(){
+        var modal = $('.modal');
+        var item = $(this).parents('.item');
         var id = $(this).data("id");
-
         $.post("/basket/add",
         {
             id: id
         },
         function(data){
+            modal.find('.modal-title').html('ADDED');
+            modal.find('.modal-body p').html(item.find('h4').html());
             $("#basket-price").html(data);
         });
     });
 
     $(".delete-from-basket").click(function(){
-        // $(this).parents('.caption').remove();
+        var modal = $('.modal');
         var item = $(this).parents('.item');
         var id = $(this).data("id");
         $.post("/basket/remove",
@@ -25,6 +28,8 @@ $(document).ready(function(){
                 quantity: 1
             },
             function(data, status){
+                modal.find('.modal-title').html('DELETED');
+                modal.find('.modal-body p').html(item.find('h4').html());
                 var json = JSON.parse(data);
                 var quantity = json['quantity'];
                 if(quantity === 0) {
