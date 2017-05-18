4de01543533cfcc32918bf80cf93f8d082074d64
Michał Osak <osakmichal@gmail.com>
2017-05-18 15:50:21 +0200

    order controller

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -1,7 +1,33 @@
 package com.codecool.shop.controller;
 
-/**
- * Created by michalosak on 18/05/2017.
- */
+import com.codecool.shop.dao.*;
+import com.codecool.shop.model.Basket;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.User;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class OrderController {
+
+    private ProductDao productDao = new ProductDaoSqlite();
+    private UserDao userDao = new UserDaoSqlite();
+    private OrderDao orderDao = new OrderDaoSqlite();
+    private BasketDao basketDao = new BasketDaoSqlite();
+
+    public String showAll(Request request, Response response) {
+
+        List<Order> orderList = orderDao.getAll();
+        System.out.println(orderList);
+        Map params = new HashMap<>();
+        params.put("orders", orderList);
+        ModelAndView render = new ModelAndView(params, "admin/orderslist");
+        return new ThymeleafTemplateEngine().render(render);
+    }
 }
