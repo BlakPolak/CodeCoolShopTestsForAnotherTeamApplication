e4dd580b1b6266b1b4908a4908c37d2f233e9df9
Michał Osak <osakmichal@gmail.com>
2017-05-18 19:22:43 +0200

    INSERTIONS to database

--- src/main/java/com/codecool/shop/dao/SqliteJDBCConnector.java
+++ src/main/java/com/codecool/shop/dao/SqliteJDBCConnector.java
@@ -101,5 +101,17 @@ public class SqliteJDBCConnector {
                 "    name VARCHAR(255) NOT NULL\n" +
                 ", description TEXT NULL)");
 
+        statement.execute("INSERT INTO users (id, first_name, last_name, adres, phone, email) VALUES (1, 'Michael', 'Osak', 'olkusz', '12345', 'osakmichal@gmail.com');");
+        statement.execute("INSERT INTO suppliers (name, description) VALUES ('audi', 'nice cars');");
+        statement.execute("INSERT INTO suppliers (name, description) VALUES ('apple', 'nice phones');");
+        statement.execute("INSERT INTO categories (name, description) VALUES ('cars', 'nice cars cat');");
+        statement.execute("INSERT INTO categories (name, description) VALUES ('phones', 'nice phones cat');");
+        statement.execute("INSERT INTO products (name, description, price, category_id, supplier_id) VALUES ('iphone', 'nice phone', 123, 1, 1);");
+        statement.execute("INSERT INTO products (name, description, price, category_id, supplier_id) VALUES ('samsung', 'nice phone', 103, 1, 1);");
+        statement.execute("INSERT INTO products (name, description, price, category_id, supplier_id) VALUES ('audi', 'nice car', 10300, 2, 2);");
+        statement.execute("INSERT INTO baskets (order_id, product_id, quantity) VALUES (1, 1, 1);");
+        statement.execute("INSERT INTO baskets (order_id, product_id, quantity) VALUES (1, 2, 1);");
+        statement.execute("INSERT INTO orders (user_id, paid, send) VALUES (1, 1, 1);");
     }
+
 }
