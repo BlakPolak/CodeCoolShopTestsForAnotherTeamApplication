e6f435bbb1327b512dd0bf0adc318d158ab2ed1c
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-18 11:14:34 +0200

    Add add method for BasketDaoSqlite

--- src/main/java/com/codecool/shop/dao/BasketDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/BasketDaoSqlite.java
@@ -1,14 +1,32 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Basket;
+import com.codecool.shop.model.BasketItem;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class BasketDaoSqlite extends BaseDao implements BasketDao{
 
     @Override
     public void add(Basket basket, Integer orderId) {
+        for (BasketItem item: basket.getItems()) {
+            try {
+                String query = "INSERT INTO baskets (order_id, product_id, quantity) VALUES (?, ?, ?)";
+                PreparedStatement statement = this.getConnection().prepareStatement(query);
+                statement.setInt(1, orderId);
+                statement.setInt(2, item.getProduct().getId());
+                statement.setInt(3, item.getQuantity());
+                statement.executeUpdate();
 
+                statement.close();
+
+
+            } catch (SQLException e ) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
