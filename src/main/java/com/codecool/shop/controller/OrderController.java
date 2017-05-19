f142877215a12784584e95d44efe4e827bb7deb9
Kamil <mika.kamil12@gmail.com>
2017-05-19 09:49:46 +0200

    Remove unused imports and fields

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.*;
-import com.codecool.shop.model.Basket;
 import com.codecool.shop.model.Order;
-import com.codecool.shop.model.User;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -16,15 +14,12 @@ import java.util.Map;
 
 public class OrderController {
 
-    private ProductDao productDao = new ProductDaoSqlite();
-    private UserDao userDao = new UserDaoSqlite();
     private OrderDao orderDao = new OrderDaoSqlite();
-    private BasketDao basketDao = new BasketDaoSqlite();
 
     public String showAll(Request request, Response response) {
 
         List<Order> orderList = orderDao.getAll();
-        Map params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("orders", orderList);
         System.out.println(orderList.get(0).getBasket().getItems());
         ModelAndView render = new ModelAndView(params, "admin/orderslist");
