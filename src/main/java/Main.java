f84eea775bd933d5ce0c06c8ecbe7706cb4e078d
Kamil <mika.kamil12@gmail.com>
2017-05-19 10:46:44 +0200

    Add dropTable method call

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,18 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length > 0 && args[0].equals("--create-tables")){
-            System.out.println("Test");
+        if(args.length > 0 && args[0].equals("--init-db")){
+            System.out.println("Created new tables");
             try{
                 SqliteJDBCConnector.createTables();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        if(args.length > 0 && args[0].equals("--migrate-db")){
+            System.out.println("Drop And create");
+            try{
+                SqliteJDBCConnector.dropTables();
             }catch (SQLException e){
                 e.printStackTrace();
             }
