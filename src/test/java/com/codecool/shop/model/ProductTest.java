cc77127eb4bcbe72c1fb60952ece79927df17f66
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-05-30 14:07:30 +0200

    Create @BeforeEach tests method setUp

--- src/test/java/com/codecool/shop/model/ProductTest.java
+++ src/test/java/com/codecool/shop/model/ProductTest.java
@@ -1,8 +1,20 @@
 package com.codecool.shop.model;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class ProductTest {
+    ProductCategory mockedProductCategory;
+    Supplier mockedSupplier;
 
+    @BeforeEach
+    public void setUp(){
+        mockedProductCategory = mock(ProductCategory.class);
+        mockedSupplier = mock(Supplier.class);
+    }
 }
