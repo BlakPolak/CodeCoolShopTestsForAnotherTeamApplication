4b0f0b08b4f800858c977e4de9b41e3ede418604
Anna Matras <ania.matras@gmail.com>
2017-05-30 10:28:48 +0000

    Add beforeEach method with object initialization

--- src/test/java/com/codecool/shop/model/ProductCategoryTest.java
+++ src/test/java/com/codecool/shop/model/ProductCategoryTest.java
@@ -1,35 +1,39 @@
 package com.codecool.shop.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryTest {
 
+    private ProductCategory category;
+
+    @BeforeEach
+    void executeBeforeEach() {
+        category = new ProductCategory("test", "test", "test");
+    }
+
     @Test
     void testSetAndGetName() {
-        ProductCategory category = new ProductCategory("dupa", "dupa", "dupa");
         category.setName("NewName");
         assertEquals("NewName", category.getName());
     }
 
     @Test
     void testSetAndGetDepartment() {
-        ProductCategory category = new ProductCategory("dupa", "dupa", "dupa");
         category.setDepartment("NewDepartment");
         assertEquals("NewDepartment", category.getDepartment());
     }
 
     @Test
     void testSetAndGetDescription() {
-        ProductCategory category = new ProductCategory("dupa", "dupa", "dupa");
         category.setDescription("NewDescription");
         assertEquals("NewDescription", category.getDescription());
     }
 
     @Test
-    public void testFailIfAddProductTakeNull() {
-        ProductCategory category = new ProductCategory("dupa", "dupa", "dupa");
+    void testFailIfAddProductTakeNull() {
         assertThrows(NullPointerException.class, () -> {
             category.addProduct(null);
         });
