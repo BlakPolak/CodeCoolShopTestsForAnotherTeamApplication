8f2bef0967f523f6f54bf6513b099245a4b9fd4a
mari <maristein3@gmail.com>
2017-05-30 12:24:04 +0200

    Create BasketTest class and setup method

---
+++ src/test/java/com/codecool/shop/model/BasketTest.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class BasketTest {
+    private Basket basket;
+    private ProductCategory category;
+    private Supplier supplier;
+    private Product product;
+    private List<BasketItem> items;
+    private BasketItem item;
+
+    @BeforeEach
+    void setup() {
+        this.basket = new Basket();
+        this.category = new ProductCategory("","","");
+        this.supplier = new Supplier("", "");
+        this.product = new Product("", 1.000000001f, "", "", category, supplier);
+        this.items = new ArrayList<>();
+        this.item = new BasketItem(product, 1);
+    }
+}
