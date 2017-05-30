a9996c7deb25f99c2734bb7d7d93ce40eae168fa
Anna Matras <ania.matras@gmail.com>
2017-05-30 15:03:21 +0000

    Add test to set and get products method

--- src/test/java/com/codecool/shop/model/ProductCategoryTest.java
+++ src/test/java/com/codecool/shop/model/ProductCategoryTest.java
@@ -3,15 +3,23 @@ package com.codecool.shop.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryTest {
 
     private ProductCategory category;
+    private Product product;
+    private Supplier supplier;
+    private ArrayList<Product> products;
 
     @BeforeEach
     void executeBeforeEach() {
+        supplier = new Supplier("", "");
         category = new ProductCategory("test", "test", "test");
+        product = new Product("test", 1.000f, "PLN", "test", category, supplier);
+        products = new ArrayList<>();
     }
 
     @Test
@@ -30,6 +38,13 @@ class ProductCategoryTest {
     void testSetAndGetDescription() {
         category.setDescription("NewDescription");
         assertEquals("NewDescription", category.getDescription());
+    }
+
+    @Test
+    void testSetAndGetProducts() {
+        products.add(product);
+        category.setProducts(products);
+        assertEquals(products, category.getProducts());
     }
 
     @Test
