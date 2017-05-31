722118dc6602c100c825443c48b9a84d0e8e3ec3
pawellasota <plasota76@gmail.com>
2017-05-31 11:42:08 +0200

    Add OrderTest class with setUp()

---
+++ src/test/java/com/codecool/shop/model/OrderTest.java
@@ -0,0 +1,20 @@
+package com.codecool.shop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class OrderTest {
+    private Basket basket;
+    private User user;
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        this.order = new Order(12);
+        this.basket = mock(Basket.class);
+        this.user = mock(User.class);
+    }
+}
