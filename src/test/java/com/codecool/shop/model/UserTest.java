de0017d7b17af31510a1534f98c6aa1cbb4712d0
Anna Matras <ania.matras@gmail.com>
2017-05-31 09:16:44 +0000

    Add test to set user id

---
+++ src/test/java/com/codecool/shop/model/UserTest.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class UserTest {
+
+        private User user;
+
+        @BeforeEach
+        void setup() {
+            user = new User("test", "test", "test", "test", "test");
+        }
+
+        @Test
+        void testSetAndGetId() {
+            user.setId(-1);
+            assertEquals(-1, user.getId(), 0.0000001);
+        }
+    }
