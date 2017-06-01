1defacd115dd0fc0f4653ada6c9d1fef7ba294d8
mari <maristein3@gmail.com>
2017-06-01 12:19:56 +0200

    Adjust connenction

--- src/test/java/com/codecool/shop/dao/OrderDaoSqliteTest.java
+++ src/test/java/com/codecool/shop/dao/OrderDaoSqliteTest.java
@@ -1,21 +1,32 @@
 package com.codecool.shop.dao;
-
-
+import com.codecool.shop.model.Order;
+import db.TestSqliteJDBCConnector;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class OrderDaoSqliteTest {
     private OrderDaoSqlite orderDaoSq;
-    Connection connection;
+    private Connection connection;
 
     @BeforeEach
-    public void setup() {
-        this.orderDaoSq = new OrderDaoSqlite(connection);
+    public void setup() throws SQLException, IOException {
+        connection = DriverManager.getConnection("jdbc:sqlite:src/test/java/db/test.db");
+        orderDaoSq = new OrderDaoSqlite(connection);
+        TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/BaseStructure.sql");
+    }
+
+    @AfterEach
+    void closeConnection() throws SQLException {
+        connection.close();
     }
 
     @Test
@@ -30,7 +41,8 @@ class OrderDaoSqliteTest {
 
     @Test
     public void testGetAllReturnsExpectedList() {
-        assertEquals(null, orderDaoSq.getAll());
+        List<Order> orders = new ArrayList<>();
+        assertEquals(orders, orderDaoSq.getAll());
     }
 
     @Test
