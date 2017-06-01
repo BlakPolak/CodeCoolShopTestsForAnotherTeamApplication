2fa40ceb7fad9525f83526f7db14d7b803b34c6b
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-01 22:35:56 +0200

    Add test if render show product show expected ModelAndView

--- src/test/java/com/codecool/shop/controller/ProductControllerTest.java
+++ src/test/java/com/codecool/shop/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Basket;
 import com.codecool.shop.model.Product;
 import db.TestSqliteJDBCConnector;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,17 +30,32 @@ class ProductControllerTest {
     Session session;
     Connection connection;
     ProductDao productDao;
+    Basket basket;
 
     @BeforeEach
     void setup() throws IOException, SQLException {
         connection = DriverManager.getConnection("jdbc:sqlite:src/test/java/db/test.db");
         TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/BaseStructure.sql");
+        TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/ProductControllerTest.sql");
         request = mock(Request.class, Mockito.RETURNS_DEEP_STUBS);
         response = mock(Response.class, Mockito.RETURNS_DEEP_STUBS);
         productController = new ProductController(connection);
         productDao = mock(ProductDao.class);
+        basket = mock(Basket.class);
         session = mock(Session.class);
     }
 
+    @Test
+    void testIfRenderShowProductShowAllExpectedModelAndView () {
+        when(request.session()).thenReturn(session);
+        when(session.attribute("basket")).thenReturn(basket);
+        when(request.params("id")).thenReturn("1");
+        Product product = productDao.find(1);
+        Map<String, Object> params = new HashMap<>();
+        params.put("product", product);
+        params.put("basket", basket);
+        ModelAndView modelAndView = new ModelAndView(params, "product/product");
+        assertSame(modelAndView.getViewName(), productController.showProduct(request, response).getViewName());
+    }
 
 }
