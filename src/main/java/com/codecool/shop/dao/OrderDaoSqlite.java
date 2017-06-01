d334262795129d40960b36c97e66ab506c4a8856
Anna Matras <ania.matras@gmail.com>
2017-06-01 19:20:57 +0000

    Remove unused print

--- src/main/java/com/codecool/shop/dao/OrderDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/OrderDaoSqlite.java
@@ -81,7 +81,6 @@ public class OrderDaoSqlite extends BaseDao implements OrderDao{
             Connection connection = this.getConnection();
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(SELECTALL);
-
             orders = createOrdersList(rs);
 
             rs.close();
