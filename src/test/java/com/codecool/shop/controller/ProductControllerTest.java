019159a78e82bb0b1c2e87b870acaeac29fa7376
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-02 01:27:57 +0200

    Clean code

--- src/test/java/com/codecool/shop/controller/ProductControllerTest.java
+++ src/test/java/com/codecool/shop/controller/ProductControllerTest.java
@@ -26,7 +26,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ProductControllerTest {
-
     Request request;
     Response response;
     ProductController productController;
@@ -34,8 +33,6 @@ class ProductControllerTest {
     Connection connection;
     ProductDao productDao;
     Basket basket;
-    ProductCategoryDao productCategoryDao;
-    SupplierDaoSqlite supplierDao;
 
 
     @BeforeEach
@@ -45,10 +42,10 @@ class ProductControllerTest {
         TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/ProductControllerTest.sql");
         request = mock(Request.class, Mockito.RETURNS_DEEP_STUBS);
         response = mock(Response.class, Mockito.RETURNS_DEEP_STUBS);
+        session = mock(Session.class);
+        basket = mock(Basket.class);
         productController = new ProductController(connection);
         productDao = mock(ProductDao.class);
-        basket = mock(Basket.class);
-        session = mock(Session.class);
     }
 
     @Test
@@ -64,24 +61,29 @@ class ProductControllerTest {
         SupplierDaoSqlite supplierDao = new SupplierDaoSqlite(connection);
         ProductDaoSqlite productDaoSqlite = new ProductDaoSqlite(connection);
         ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite(connection);
+
         when(request.queryParams("name")).thenReturn("name");
         when(request.queryParams("price")).thenReturn("20");
         when(request.queryParams("supplier")).thenReturn("1");
         when(request.queryParams("category")).thenReturn("1");
         when(request.params(":id")).thenReturn("1");
+
         String name = request.queryParams("name");
         String price = request.queryParams("price");
         String description = request.queryParams("name");
         String id = request.params(":id");
         String currency = "PLN";
+
         Supplier supplier = supplierDao.find(Integer.parseInt(id));
         ProductCategory productCategory = productCategoryDao.find(Integer.parseInt(id));
         Product product = new Product(name, Float.parseFloat(price), currency, description, productCategory, supplier);
         productDaoSqlite.update(product);
+
         Map<String, Object> params = new HashMap<>();
         params.put("product", productDao.find(Integer.parseInt(id)));
         params.put("suppliers", supplierDao.getAll());
         params.put("categories", productCategoryDao.getAll());
+
         ModelAndView modelAndView = new ModelAndView(params, "admin/productEdit");
         assertSame(modelAndView.getViewName(), productController.adminProductEdit(request, response).getViewName());
     }
@@ -91,23 +93,28 @@ class ProductControllerTest {
         SupplierDaoSqlite supplierDao = new SupplierDaoSqlite(connection);
         ProductDaoSqlite productDaoSqlite = new ProductDaoSqlite(connection);
         ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite(connection);
+
         when(request.queryParams("name")).thenReturn("name");
         when(request.queryParams("price")).thenReturn("20");
         when(request.queryParams("supplier")).thenReturn("1");
         when(request.queryParams("category")).thenReturn("1");
         when(request.params("id")).thenReturn("1");
+
         String name = request.queryParams("name");
         String price = request.queryParams("price");
         String description = request.queryParams("name");
         String id = request.params("id");
         String currency = "PLN";
+
         Supplier supplier = supplierDao.find(Integer.parseInt(id));
         ProductCategory productCategory = productCategoryDao.find(Integer.parseInt(id));
         Product product = new Product(name, Float.parseFloat(price), currency, description, productCategory, supplier);
         productDaoSqlite.insert(product);
+
         Map<String, Object> params = new HashMap<>();
         params.put("categories", productCategoryDao.getAll());
         params.put("suppliers", supplierDao.getAll());
+
         ModelAndView modelAndView = new ModelAndView(params, "admin/productAdd");
         assertSame(modelAndView.getViewName(), productController.adminProductInsert(request, response).getViewName());
     }
@@ -118,10 +125,12 @@ class ProductControllerTest {
         when(request.session()).thenReturn(session);
         when(session.attribute("basket")).thenReturn(basket);
         when(request.params("id")).thenReturn("1");
+
         Product product = productDao.find(1);
         Map<String, Object> params = new HashMap<>();
         params.put("product", product);
         params.put("basket", basket);
+
         ModelAndView modelAndView = new ModelAndView(params, "product/product");
         assertSame(modelAndView.getViewName(), productController.showProduct(request, response).getViewName());
     }
