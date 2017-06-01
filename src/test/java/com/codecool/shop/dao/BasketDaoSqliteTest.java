77c4f749597649e6c60ba00da240a482c8bb7bc0
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-01 15:11:19 +0200

    Adds needed imports, @BeforeEach and @AfterEach

--- src/test/java/com/codecool/shop/dao/BasketDaoSqliteTest.java
+++ src/test/java/com/codecool/shop/dao/BasketDaoSqliteTest.java
@@ -1,8 +1,40 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.*;
+import db.TestSqliteJDBCConnector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 class BasketDaoSqliteTest {
+
+    Connection connection;
+    BasketDaoSqlite basketDaoSqlite;
+
+
+    @BeforeEach
+    public void setUp() throws SQLException, IOException {
+        connection = DriverManager.getConnection("jdbc:sqlite:src/test/java/db/test.db");
+        TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/BaseStructure.sql");
+        TestSqliteJDBCConnector.runSql(connection, "src/test/java/db/scripts/BasketDaoSqliteTest.sql");
+        basketDaoSqlite = new BasketDaoSqlite(connection);
+
+    }
+
+    @AfterEach
+    void closeConnection() throws SQLException {
+        connection.close();
+    }
+
 
 }
